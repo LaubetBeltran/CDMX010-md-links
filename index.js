4d4d42bd33da6f5bdde1c6f6396a5f1378a1db70
LaubetBeltran <laubetpop@gmail.com>
2021-03-18 22:40:20 -0600

    Primera función para comprobar ai un archivo tuene la terminación md/// utilizando el módulo fs node.js para leer un archivo

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+
+const ReadDoc = (doc) => {
+	const mdDocFilter = doc.endsWith('.md');
+  if (mdDocFilter === true) {
+		const docContent = fs.readFileSync(doc, 'utf8');
+		console.log(docContent);
+	} else {
+		console.log('no soy un archivo .md');
+	}
+}
+
+//ReadDoc('README.md');
+ReadDoc('docPrueba.txt');
