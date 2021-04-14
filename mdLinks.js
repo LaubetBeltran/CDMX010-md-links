a65614ea709aaaf7b6e38b0a658d3a2fe89f5c79
LaubetBeltran <laubetpop@gmail.com>
2021-04-13 21:18:36 -0500

    intentando promesa recursiva

--- mdLinks.js
+++ mdLinks.js
@@ -4,7 +4,6 @@ const path = require('path');
 const objFunctions = require('./mdLinksFunctions.js');
 const showFunctions = require('./showCliFunctions.js');
 
-
 const mdLinks = (archive, {validated}={}) => {
 	return new Promise((resolve,reject) => {
 		let validation = validated === true ? true : false;
@@ -21,7 +20,7 @@ const mdLinks = (archive, {validated}={}) => {
 					}
 				} else if (fsStat.isDirectory()) {
 					showFunctions.showArchivePath(archive);
-					readDirectory(archive, validation)
+					resolve(readDirectory(archive, validation))
 				} else {(showFunctions.showArchivePath(archive))};
 			}
 		});
@@ -42,6 +41,58 @@ const readDirectory = (directory, validation) => {
 }
 
 module.exports= mdLinks;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mdLinks = (archive, {validated}={}) => {
+// 	return new Promise((resolve,reject) => {
+// 		let validation = validated === true ? true : false;
+// 		const extNamePath = path.extname(archive);
+// 		fs.lstat(archive, (err, fsStat) => {
+// 			if (err) {
+// 				return console.log(err);
+// 			} else {
+// 				if (fsStat.isFile()){
+// 					if(extNamePath === '.md'){ 
+// 					objFunctions.readDocMd(archive, validation)
+// 						.then((arrInfoLinksObjects)=> resolve(arrInfoLinksObjects))
+// 						.catch((error) => reject(error))
+// 					}
+// 				} else if (fsStat.isDirectory()) {
+// 					showFunctions.showArchivePath(archive);
+// 					readDirectory(archive, validation)
+// 				} else {(showFunctions.showArchivePath(archive))};
+// 			}
+// 		});
+// 	});
+// }
+
+// const readDirectory = (directory, validation) => {
+// 	fs.readdir(directory, (err, files) => {
+// 		if (err) {
+// 			return console.log(chalk.red.bold('Error al procesar el archivo'));
+// 		}	else {
+// 			files.forEach((doc) => {
+// 				const newPath = path.normalize(directory + '/' + doc);
+// 				mdLinks(newPath, {validated: validation});
+// 			}); 
+// 		}
+// 	});
+// }
+
+// module.exports= mdLinks;
 
 
 
